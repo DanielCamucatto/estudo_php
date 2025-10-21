f75bac9875002d047de5e15ad45a8a529b13a86c
danielCamucatto <daniel.camucatto@visto.global>
2025-10-21 08:51:10 -0300

    fix: corrige a lógica de abertura e leitura do arquivo CSV para calcular o valor total do estoque

--- src/ex020/index.php
+++ src/ex020/index.php
@@ -16,23 +16,30 @@ file_put_contents($caminho_arquivo, "Nome,Categoria,Preco,Quantidade\nLaptop,Ele
 // 1. Abra o arquivo...
 $inventarioCSV = fopen($caminho_arquivo, "r");
 
+if($inventarioCSV === false){
+   die("Erro não foi possível abrir o arquivo CSV" . $caminho_arquivo);
+}
 // 2. Itere sobre as linhas. Dica: Use fgetcsv() uma vez antes do loop 
 // para ignorar a primeira linha (o cabeçalho).
-if($inventarioCSV === false){
-    return null;
-}
+fgetcsv($inventarioCSV);
 
 while(($dados = fgetcsv($inventarioCSV, 1000, " , ")) !== false)
 {
-    $soma = 0;
-    $soma = $inventarioCSV;
+   // indice do array de dados
+   // [0] = nome
+   // [1] = categoria
+   // [2] = preco
+   // [3] = quantidade
 
-    return $soma;
+   // 3. No loop, calcule o valor do estoque.
+   $preco = (float) $dados[2];
+   $quantidade = (int) $dados[3];
+
+   $valor_total_estoque += ($preco * $quantidade);
 }
 
-// 3. No loop, calcule o valor do estoque.
 
 // 4. Feche o arquivo.
 fclose($inventarioCSV);
 // 5. Imprima o resultado final.
-echo $soma;
+echo "O valor total do estoque é R$: " . number_format($valor_total_estoque, 2, ",", ".");
