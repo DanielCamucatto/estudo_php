95cefb10573e9902381958f9787f46cf800e7ef8
danielCamucatto <daniel.camucatto@visto.global>
2025-10-21 17:49:44 -0300

    feat: ajusta a decodificação do JSON e calcula o valor total do estoque

--- src/ex022/index.php
+++ src/ex022/index.php
@@ -11,19 +11,51 @@ $carrinho_json = '[
 $carrinho_array = []; 
 // Variável para armazenar o JSON de resposta
 $json_saida = '';
+ $valor_total_estoque = 0.0;
 
 // SEU CÓDIGO COMEÇA AQUI:
 
 // A. Decodificar o JSON de entrada para um array PHP.
-$json_entrada = json_decode($carrinho_json);
+$json_entrada = json_decode($carrinho_json, true);                  
+// Imprimir a estrutura decodificada de forma legível no navegador.
+// json_decode retorna um objeto (stdClass) por padrão, e não pode ser
+// convertido diretamente para string com echo. Use print_r/var_dump
+// ou converta de volta para JSON com json_encode.
+// echo '<pre>';
+// print_r($json_entrada);
+// echo '</pre>';
+
+// Alternativas:
+// var_dump($json_entrada);
+// echo json_encode($json_entrada, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+// echo $carrinho_json; // imprime a string JSON original
 
 // B. Loop para iterar e calcular o subtotal de cada item (preco * quantidade).
 // Lembre-se que $carrinho_array é um array de arrays.
 
+foreach($json_entrada as $item)
+{
+    $preco = $item['preco'];
+    $quantidade = $item['quantidade']; 
+
+    $sub_total= $preco * $quantidade;
+    $valor_total_estoque += $sub_total; 
+
+    $carrinho_array[] =[
+        'id' => $item['id'],
+        'produto' => $item['produto'],
+        'preco' => $preco,
+        'quantidade' => $quantidade,
+        'subtotal' => $sub_total
+    ];
+    
+}
 // C. Codificar o array processado de volta para JSON na variável $json_saida.
-
+$json_saida = json_encode([
+    'Itens' => $carrinho_array,
+    'Valor_total_estoque' => $valor_total_estoque,
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 // D. Imprimir o resultado final.
-
+print_r($json_saida);
 // FIM DO SEU CÓDIGO
-
 ?>
